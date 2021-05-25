404321cbb95621c8f7272d58dea67f5444938e43
vuthanh199826 <vuthanh199825@gmail.com>
2021-05-25 16:18:31 +0700

    done

---
+++ src/BubbleSortByStep.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class BubbleSortByStep {
+    public static void sort(int[] list){
+        boolean needNextPass = true;
+        for (int i = 1; i < list.length && needNextPass; i++) {
+            needNextPass = false;
+            for (int j = 0; j < list.length - i ; j++) {
+                if(list[j] > list[j+1]){
+                    int temp = list[j];
+                    list[j] = list[j+1];
+                    list[j+1] = temp;
+                    needNextPass = true;
+                }
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter list size:");
+        int size = sc.nextInt();
+        int[] list = new int[size];
+        System.out.println("Enter " + list.length + " values:");
+        for (int i = 0; i < list.length; i++) {
+            list[i] = sc.nextInt();
+        }
+        sort(list);
+        System.out.print("Your input list: ");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + "\t");
+        }
+    }
+}
